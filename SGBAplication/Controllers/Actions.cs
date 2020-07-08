3259b5a5c303a121b4093e16dc3ae6097feee6e1
varan1717 <67777080+varan1717@users.noreply.github.com>
2020-07-08 14:59:22 +0300

    Наладил Processing

--- SGBAplication/Controllers/Actions.cs
+++ SGBAplication/Controllers/Actions.cs
@@ -21,59 +21,60 @@ namespace Controllers
     /// </summary>
     public class Controller
     {
-        public static List<List<System.Numerics.Complex>> DecoderOfNonResemplingSignalWithPll(string startIndex, string fileName, 
-            ref string fullMessage, ref string country, ref string currentFrequancy, ref double std, ref double meanFreq, ref double phasa, ref double iteration)
+        public static List<List<System.Numerics.Complex>> DecoderOfNonResemplingSignalWithPll(string startIndex, string fileName, ref MathAndProcessing.OutputDataPLL dataPack)
         {
             var I = new List<double>();
             var Q = new List<double>();
+
             GetSamples(fileName, ref I, ref Q, 10000000);
             var rI = ResemplingOfSignal.GetResemplingSamples(I);
             var rQ = ResemplingOfSignal.GetResemplingSamples(Q);
-            //double std = 0;
-            //double meanFreq = 0;
-            //double phasa = 0;
-            //double iteration =0;
-            return Processing.DecoderPLL(rI, rQ, startIndex, fileName, ref fullMessage, ref country, ref currentFrequancy, ref std, ref meanFreq, ref phasa, ref iteration);
+
+            ProcessingPLL processor = new ProcessingPLL();
+            var output = processor.Decoder(rI, rQ, startIndex);
+            dataPack = processor._dataPack;
+            return output;
         }
         
-        public static List<List<System.Numerics.Complex>> DecoderOfResemplingSignalWithPll(string startIndex, string fileName,
-            ref string fullMessage, ref string country, ref string currentFrequancy, ref double std,
-            ref double meanFreq, ref double phasa, ref double iteration)
+        public static List<List<System.Numerics.Complex>> DecoderOfResemplingSignalWithPll(string startIndex, string fileName, ref MathAndProcessing.OutputDataPLL dataPack)
         {
             var rI = new List<double>();
             var rQ = new List<double>();
 
             GetSamples(fileName, ref rI, ref rQ, 76809,Convert.ToInt64(startIndex),';');
 
-            //System.Windows.Forms.string new_ind = new System.Windows.Forms.string();
-            var new_ind = "0";//поскольку прочитали уже с нужного индекса и выбрали посылку
-            return Processing.DecoderPLL(rI, rQ, new_ind, fileName, ref fullMessage, ref country, ref currentFrequancy, ref std, ref meanFreq, ref phasa, ref iteration);
-
+            ProcessingPLL processor = new ProcessingPLL();
+            var output = processor.Decoder(rI, rQ, startIndex);
+            dataPack = processor._dataPack;
+            return output;
         }
 
-        public static List<List<System.Numerics.Complex>> DecoderOfNonResemplingSignal(string startIndex, string fileName,
-           ref string fullMessage, ref string country, ref string currentFrequancy)
+        public static List<List<System.Numerics.Complex>> DecoderOfNonResemplingSignal(string startIndex, string fileName, ref MathAndProcessing.OutputData dataPack)
         {
             var I = new List<double>();
             var Q = new List<double>();
+
             GetSamples(fileName, ref I, ref Q, 10000000);
             var rI = ResemplingOfSignal.GetResemplingSamples(I);
             var rQ = ResemplingOfSignal.GetResemplingSamples(Q);
 
-            return Processing.Decoder(rI, rQ, startIndex, fileName, ref fullMessage, ref country, ref currentFrequancy);
+            Processing processor = new Processing();
+            var output = processor.Decoder(rI, rQ, startIndex);
+            dataPack = processor._dataPack;
+            return output;
         }
         
-        public static List<List<System.Numerics.Complex>> DecoderOfResemplingSignal(string startIndex, string fileName,
-            ref string fullMessage, ref string country, ref string currentFrequancy)
+        public static List<List<System.Numerics.Complex>> DecoderOfResemplingSignal(string startIndex, string fileName, ref MathAndProcessing.OutputData dataPack)
         {
             var rI = new List<double>();
             var rQ = new List<double>();
 
             GetSamples(fileName, ref rI, ref rQ, 76809,Convert.ToInt64(startIndex),';');
 
-            //System.Windows.Forms.string new_ind = new System.Windows.Forms.string();
-            var new_ind = "0";//поскольку прочитали уже с нужного индекса и выбрали посылку
-            return Processing.Decoder(rI, rQ, new_ind, fileName, ref fullMessage, ref country, ref currentFrequancy);
+            Processing processor = new Processing();
+            var output = processor.Decoder(rI, rQ, startIndex);
+            dataPack = processor._dataPack;
+            return output;
 
         }
 
---
+++ SGBAplication/MathAndProcessing/RefData/OutputData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndProcessing
+{
+    public class OutputData
+    {
+        public string fullMessage { get; set; }
+
+        public string country { get; set; }
+
+        public string currentFrequancy { get; set; }
+    }
+}
